b47875df31a5d77b4cce0f349041be59fcd458e5
jbielawski <foxandthefiddle@comcast.net>
2025-10-03 21:41:02 -0500

    feat: change img to Nextjs Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-
       <h1 className="text-6xl font-bold text-center mt-40 mb-10 mr-50 ml-50">
         I've been working on some cool stuff.
       </h1>
@@ -15,30 +15,33 @@ export default function Home() {
             href="/work/cvm"
             className="h-screen flex items-center justify-center"
           >
-            <img
-              src="web_lockup.png"
+            <Image
+              src="/web_lockup.png"
               alt="CVM Project Thumbnail"
-              className="h-95 w-auto"
+              width={500}
+              height={500}
             />
           </Link>
           <Link
             href="/work/cvm"
             className="h-screen flex items-center justify-center"
           >
-            <img
-              src="web_lockup.png"
+            <Image
+              src="/web_lockup.png"
               alt="CVM Project Thumbnail"
-              className="h-95 w-auto"
+              width={500}
+              height={500}
             />
           </Link>
           <Link
             href="/work/cvm"
             className="h-screen flex items-center justify-center"
           >
-            <img
-              src="web_lockup.png"
+            <Image
+              src="/web_lockup.png"
               alt="CVM Project Thumbnail"
-              className="h-95 w-auto"
+              width={500}
+              height={500}
             />
           </Link>
         </div>
