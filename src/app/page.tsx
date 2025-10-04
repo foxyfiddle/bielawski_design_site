bb69efefef0b54a45b01598a501aa864b07d568b
jbielawski <foxandthefiddle@comcast.net>
2025-10-04 14:05:36 -0500

    feat: add extra gallery row and image titles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,46 +4,99 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-6xl font-bold text-center text-base-content mt-40 mb-10 mr-50 ml-50">
+      <h1 className="text-6xl font-bold text-center text-base-content mt-40 mb-65 mr-50 ml-50">
         {"I've been working on some cool stuff."}
       </h1>
 
       {/* Home page gallery */}
       <div className="flex flex-col">
         <div className="flex flex-row justify-center gap-10 p-10">
-          <Link
-            href="/work/cvm"
-            className="h-screen flex items-center justify-center"
-          >
-            <Image
-              src="/web_lockup.png"
-              alt="CVM Project Thumbnail"
-              width={500}
-              height={500}
-            />
-          </Link>
-          <Link
-            href="/work/cvm"
-            className="h-screen flex items-center justify-center"
-          >
-            <Image
-              src="/web_lockup.png"
-              alt="CVM Project Thumbnail"
-              width={500}
-              height={500}
-            />
-          </Link>
-          <Link
-            href="/work/cvm"
-            className="h-screen flex items-center justify-center"
-          >
-            <Image
-              src="/web_lockup.png"
-              alt="CVM Project Thumbnail"
-              width={500}
-              height={500}
-            />
-          </Link>
+          <div className="flex flex-col items-start">
+            <Link
+              href="/work/cvm"
+              className="flex items-center justify-center"
+            >
+              <Image
+                src="/web_lockup.png"
+                alt="CVM Project Thumbnail"
+                width={500}
+                height={500}
+              />
+            </Link>
+            <p className="pt-7">Cuyahoga Valley Media</p>
+          </div>
+          <div className="flex flex-col items-start">
+            <Link
+              href="/work/cvm"
+              className="flex items-center justify-center"
+            >
+              <Image
+                src="/web_lockup.png"
+                alt="CVM Project Thumbnail"
+                width={500}
+                height={500}
+              />
+            </Link>
+            <p className="pt-7">Cuyahoga Valley Media</p>
+          </div>
+          <div className="flex flex-col items-start">
+            <Link
+              href="/work/cvm"
+              className="flex items-center justify-center"
+            >
+              <Image
+                src="/web_lockup.png"
+                alt="CVM Project Thumbnail"
+                width={500}
+                height={500}
+              />
+            </Link>
+            <p className="pt-7">Cuyahoga Valley Media</p>
+          </div>
+        </div>
+        <div className="flex flex-row justify-center gap-10 p-10">
+          <div className="flex flex-col items-start">
+            <Link
+              href="/work/cvm"
+              className="flex items-center justify-center"
+            >
+              <Image
+                src="/web_lockup.png"
+                alt="CVM Project Thumbnail"
+                width={500}
+                height={500}
+              />
+            </Link>
+            <p className="pt-7">Cuyahoga Valley Media</p>
+          </div>
+          <div className="flex flex-col items-start">
+            <Link
+              href="/work/cvm"
+              className="flex items-center justify-center"
+            >
+              <Image
+                src="/web_lockup.png"
+                alt="CVM Project Thumbnail"
+                width={500}
+                height={500}
+              />
+            </Link>
+            <p className="pt-7">Cuyahoga Valley Media</p>
+          </div>
+          <div className="flex flex-col items-start">
+            <Link
+              href="/work/cvm"
+              className="flex items-center justify-center"
+            >
+              <Image
+                src="/web_lockup.png"
+                alt="CVM Project Thumbnail"
+                width={500}
+                height={500}
+              />
+            </Link>
+            <p className="pt-7">Cuyahoga Valley Media</p>
+          </div>
         </div>
       </div>
     </main>
